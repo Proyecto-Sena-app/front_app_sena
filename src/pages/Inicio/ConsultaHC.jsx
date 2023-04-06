134fdd5bdf0340cdd740ef510513fc524fdec29e
Juan Diego Calvo Hoyos <you@example.com>
2023-04-06 13:37:14 -0500

    Genración vista COnsulta hC

--- src/pages/Inicio/ConsultaHC.jsx
+++ src/pages/Inicio/ConsultaHC.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import '../../styles/Inicio.css'
 
 const ConsultaHC = () => {
   return (
-    <div>
-        <div>hola</div>
+    <div className='consulta'>
+        <h1 className='h1x'>Consulta Historia Clinica del Paciente</h1>
+        <div className='div-label'>
+          <label htmlFor="Documento" className='labelx'>N° de Documento</label>
+          <input />
+        </div>
+        <table >
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Numero de Documento</th>
+            <th>Correo</th>
+            <th>Accion</th>
+          </tr>
+          <tr>
+            <td>Juan</td>
+            <td>Calvo</td>
+            <td>12345</td>
+            <td>Juan@gmail.com</td>
+            <td>
+              <button>Descargar</button>
+              <button>Editar</button>
+            </td>
+          </tr>
+        </table>
     </div>
   )
 }
